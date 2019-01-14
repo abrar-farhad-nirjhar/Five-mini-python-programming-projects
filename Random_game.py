e52379b537c4648b12a7b7e68d8f012d9fcd3f8f
Abrar Farhad <abrar.farhad.af@gmail.com>
2019-01-14 15:02:48 +0600

    Random Game Slight Modification

--- Random_game.py
+++ Random_game.py
@@ -28,5 +28,20 @@ def main():
 
 
     
+game = True
 
-main()
+while game:
+    main()
+
+    print("Do you want to play again y/n")
+    
+    while True:
+        choice = input()
+        if choice == "y":
+            game = True
+            break
+        elif choice == "n":
+            game = False
+            break
+        else:
+            print("Please enter y or n.")
